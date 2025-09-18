9006c0171889abd6099c005ae3139aa06547edec
Baizid MD Ashadzzaman <baizid.md.ashadzzaman@gmail.com>
2025-09-19 00:02:11 +0600

    Tutorial 4

--- Tutorial-1/Program.cs
+++ Tutorial-1/Program.cs
@@ -1,8 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-string name = "asadzaman";
-int a = 10;
-int b = 20;
-int sum = a + b;    
-Console.WriteLine($"Hello, My name is {name}!");
-Console.WriteLine($"The sum of {a} and {b} is {sum}!");
-//Console.WriteLine("Hello, My name is asadzaman!");
+﻿// // See https://aka.ms/new-console-template for more information
+// string name = "ASAD ZAMAN";
+// int a = 10;
+// int b = 20;
+// int sum = a + b;    
+// Console.WriteLine($"Hello, My name is {name}!");
+// Console.WriteLine($"The sum of {a} and {b} is {sum}!");
+// //Console.WriteLine("Hello, My name is asadzaman!");
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        System.Console.WriteLine("Hello, World2!");
+    }
+}
