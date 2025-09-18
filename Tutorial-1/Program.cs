7dbc5a2f807fd111982ac5f19092e475a523a60d
Baizid MD Ashadzzaman <baizid.md.ashadzzaman@gmail.com>
2025-09-18 23:16:06 +0600

    first commit

---
+++ Tutorial-1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "asadzaman";
+int a = 10;
+int b = 20;
+int sum = a + b;    
+Console.WriteLine($"Hello, My name is {name}!");
+Console.WriteLine($"The sum of {a} and {b} is {sum}!");
+//Console.WriteLine("Hello, My name is asadzaman!");
